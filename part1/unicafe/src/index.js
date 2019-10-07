263f5a643229abe41af4ec5c8729620fcad4df8d
Saleh Rahimzadeh <saleh-rz@outlook.com>
2019-10-07 20:21:20 +0330

    part1: 6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,74 @@
+/*
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#  Created by Saleh Rahimzadeh                                                 #
+#  Copyright (C) 2019                                                          #
+#  https://saleh.sleek.page                                                    #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+*/
+
+import React, { useState } from 'react'
+import ReactDOM            from 'react-dom'
+
+console.log('Part1: 6')
+console.log(`Starting Application [${(new Date()).toLocaleTimeString()}]`)
+
+
+
+const App = () => {
+
+  /* States */
+  const [good, setGood]       = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad]         = useState(0)
+
+
+  /* Event handlers */
+  const setGoodHandler = () => {
+    setGood(good + 1)
+  }
+
+  const setNeutralHandler = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const setBadHandler = () => {
+    setBad(bad + 1)
+  }
+
+
+  console.log('Rendering Application...')
+
+  return (
+    <>
+      <h1>unicafe</h1>
+
+      <h2>Give feedback</h2>
+      <button onClick={setGoodHandler}>
+        Good
+      </button>
+      <button onClick={setNeutralHandler}>
+        Neutral
+      </button>
+      <button onClick={setBadHandler}>
+        Bad
+      </button>
+
+      <h2>Statistics</h2>
+      <p>
+        Good: {good}
+        <br />
+        Neutral: {neutral}
+        <br />
+        Bad: {bad}
+      </p>
+    </>
+  )
+
+}
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
