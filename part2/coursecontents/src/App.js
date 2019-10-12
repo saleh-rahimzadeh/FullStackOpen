4c37eeba161fc33e972613e24f14e4211a6ef7e6
Saleh Rahimzadeh <saleh-rz@outlook.com>
2019-10-12 18:54:46 +0330

    Part2: 5

--- part2/coursecontents/src/App.js
+++ part2/coursecontents/src/App.js
@@ -1,58 +1,12 @@
 import React  from 'react';
+import CoursesArray from './components/CoursesArray'
 import Course from './components/Course'
 
 
 
 const App = () => {
 
-  // Data difinitions
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-
-  const rows = () => courses.map(item => <Course key={item.id} course={item} />)
+  const rows = () => CoursesArray.map(item => <Course key={item.id} course={item} />)
 
 
   console.log('Rendering Application...')
--- part2/coursecontents/src/index.js
+++ part2/coursecontents/src/index.js
@@ -12,7 +12,7 @@ import App      from './App'
 
 
 
-console.log('Part2: 4')
+console.log('Part2: 5')
 console.log('Starting Application [' + (new Date()).toLocaleTimeString() + ']')
 
 
