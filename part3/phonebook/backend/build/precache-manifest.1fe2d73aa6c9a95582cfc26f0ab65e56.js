a6a28d8b95d4fbbf4d37d188d9ffe967b7570517
Saleh Rahimzadeh <saleh-rz@outlook.com>
2019-10-30 11:38:09 +0330

    Part3: 21

---
+++ part3/phonebook/backend/build/precache-manifest.1fe2d73aa6c9a95582cfc26f0ab65e56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a075a90150c4b2ecc77433a59a464519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d42797407afe57e64d",
+    "url": "/static/css/main.c4d05a5f.chunk.css"
+  },
+  {
+    "revision": "65c3d240a25d8bd04846",
+    "url": "/static/js/2.af3d3b42.chunk.js"
+  },
+  {
+    "revision": "74d42797407afe57e64d",
+    "url": "/static/js/main.7185d530.chunk.js"
+  },
+  {
+    "revision": "5e37706f419562cab916",
+    "url": "/static/js/runtime-main.ec2c0df5.js"
+  }
+]);
