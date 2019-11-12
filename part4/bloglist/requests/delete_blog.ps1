cac2232d543708134388d273fbc74ab42952e3ef
Saleh Rahimzadeh <saleh-rz@outlook.com>
2019-11-12 23:03:44 +0330

    Part4: 21

--- part4/bloglist/requests/delete_blog.ps1
+++ part4/bloglist/requests/delete_blog.ps1
@@ -1,2 +1,3 @@
-$id = '5dc5302e67606d07840d304a'
-irm http://localhost:3003/api/blogs/$id -Method Delete
+$id = '5dcaf321db145e1a9078b823'
+$sectoken = Read-Host "Enter Authorization Token" -AsSecureString
+irm http://localhost:3003/api/blogs/$id -Method Delete -AllowUnencryptedAuthentication -Authentication Bearer -Token $sectoken
