39032203bfe2f1cdcc349a7ca4793271574dd74f
Saleh Rahimzadeh <saleh-rz@outlook.com>
2019-11-27 19:53:49 +0330

    Part5: Amend

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -7,46 +7,46 @@ import SimpleBlog            from './SimpleBlog'
 
 describe('<SimpleBlog />', () => {
 
-    let component
-    const likesMockHandler = jest.fn()
+  let component
+  const likesMockHandler = jest.fn()
 
-    beforeEach(() => {
-        const blogData = {
-            title:  "Title1",
-            author: "Author1",
-            likes:  1
-        }
+  beforeEach(() => {
+    const blogData = {
+      title:  'Title1',
+      author: 'Author1',
+      likes:  1
+    }
 
-        component = render(
-            <SimpleBlog blog={blogData} onClick={likesMockHandler} />
-        )
-    })
+    component = render(
+      <SimpleBlog blog={blogData} onClick={likesMockHandler} />
+    )
+  })
 
 
-    test('renders its children', () => {
-        const blog = component.container.querySelector('.blog')
-        expect(blog).toBeDefined()
-        console.log(prettyDOM(blog))
-    })
+  test('renders its children', () => {
+    const blog = component.container.querySelector('.blog')
+    expect(blog).toBeDefined()
+    console.log(prettyDOM(blog))
+  })
 
-    test('renders content', () => {
-        expect(component.container.querySelector('.blog-content')).toHaveTextContent(
-            'Title1 Author1'
-        )
-    })
+  test('renders content', () => {
+    expect(component.container.querySelector('.blog-content')).toHaveTextContent(
+      'Title1 Author1'
+    )
+  })
 
-    test('renders likes', () => {
-        expect(component.container).toHaveTextContent(
-            'blog has 1 likes'
-        )
-    })
+  test('renders likes', () => {
+    expect(component.container).toHaveTextContent(
+      'blog has 1 likes'
+    )
+  })
 
-    test('twice clicking the button calls event handler twice', async () => {
-        const button = component.getByText('like+')
-        fireEvent.click(button)
-        fireEvent.click(button)
+  test('twice clicking the button calls event handler twice', async () => {
+    const button = component.getByText('like+')
+    fireEvent.click(button)
+    fireEvent.click(button)
 
-        expect(likesMockHandler.mock.calls.length).toBe(2)
-    })
+    expect(likesMockHandler.mock.calls.length).toBe(2)
+  })
 
 })
