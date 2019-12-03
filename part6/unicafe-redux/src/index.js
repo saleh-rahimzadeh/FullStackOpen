1ed4e3c6f34f13ee334ea9c429848202667d5251
Saleh Rahimzadeh <saleh-rz@outlook.com>
2019-12-03 20:08:47 +0330

    Part6: 2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -12,15 +12,41 @@ const App = () => {
     })
   }
 
+  const neutral = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+
+  const total = store.getState().good + store.getState().ok + store.getState().bad
+  const average = store.getState().good - store.getState().bad
+
+
   return (
     <div>
       <button onClick={good}>good</button>
-      <button>neutral</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <button onClick={neutral}>neutral</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>Good {store.getState().good}</div>
+      <div>Neutral {store.getState().ok}</div>
+      <div>Bad {store.getState().bad}</div>
+      <div>Total {total}</div>
+      <div>Average {total > 0 ? average / total : '-'}</div>
+      <div>Positive {total > 0 ? `${100 * store.getState().good / total} %` : '-'}</div>
     </div>
   )
 }
