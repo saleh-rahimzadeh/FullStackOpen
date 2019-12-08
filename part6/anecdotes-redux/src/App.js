a7a88c69cfd56f4cfceae41155fcf2ccbd23b20f
Saleh Rahimzadeh <saleh-rz@outlook.com>
2019-12-08 19:51:19 +0330

    Part6: 16

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -1,11 +1,21 @@
-import React        from 'react'
-import AnecdoteForm from './components/AnecdoteForm'
-import AnecdoteList from './components/AnecdoteList'
-import Notification from './components/Notification'
-import Filter       from './components/Filter'
+import React            from 'react'
+import { useEffect }    from 'react'
+import { connect }      from 'react-redux'
+import AnecdoteForm     from './components/AnecdoteForm'
+import AnecdoteList     from './components/AnecdoteList'
+import Notification     from './components/Notification'
+import Filter           from './components/Filter'
+import anecdotesService from './services/anecdotes'
+import { doInitialize } from './reducers/anecdoteReducer'
 
 
-const App = () => {
+const App = (props) => {
+  useEffect(() => {
+    anecdotesService.getAll().then(data => { 
+      props.doInitialize(data) 
+    })
+  }, [props])
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -18,4 +28,9 @@ const App = () => {
 }
 
 
-export default App
+export default connect(
+  null, 
+  { 
+    doInitialize 
+  }
+)(App)
