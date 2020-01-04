70c3a6da636c38f10fc59425af748c0dfb61fa30
Saleh Rahimzadeh <saleh-rz@outlook.com>
2020-01-04 20:49:56 +0330

    Part8: 8

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+
+
+const QUERY_ALL_AUTHORS = gql`
+  {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+
+  const authors = useQuery(QUERY_ALL_AUTHORS)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'} result={authors}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
