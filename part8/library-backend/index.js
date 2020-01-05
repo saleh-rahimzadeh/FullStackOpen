353665691e0f0b88331e9abb91da9b42472a2234
Saleh Rahimzadeh <saleh-rz@outlook.com>
2020-01-05 23:19:14 +0330

    Part8: 13

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,93 +1,31 @@
 const { ApolloServer, gql, UserInputError } = require('apollo-server')
 const uuid = require('uuid/v1')
+const mongoose = require('mongoose')
+const Book = require('./models/Book')
+const Author = require('./models/Author')
 
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+// Connection to the Database
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/app-library'
+console.info('Connecting to:', MONGODB_URI)
+mongoose.set('useFindAndModify', false)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.info('Connected to MongoDB.')
+  })
+  .catch((error) => {
+    console.error('ERROR connecting to MongoDB:', error.message)
+  })
 
 
-
+// GraphQL Schema
 const typeDefs = gql`
   type Book {
     title: String!
-    author: String
-    published: Int
-    genres: [String!]
+    author: Author!
+    published: Int!
+    genres: [String!]!
+    id: ID!
   }
 
   type Author {
@@ -141,18 +79,23 @@ const resolvers = {
       }))
   },
   Mutation: {
-    addBook: (root, args) => {
-      if (books.find(b => b.title === args.title)) {
-        throw new UserInputError('Title must be unique', { invalidArgs: args.title })
-      }
-      if (!authors.find(a => a.name === args.author)) {
-        authors = authors.concat({
-          name: args.author,
-          id: uuid()
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
+        author = new Author({ 
+          name: args.author
         })
+        author = await author.save()
       }
-      const book = { ...args, id: uuid() }
-      books = books.concat(book)
+
+      const book = new Book({ 
+        title: args.title, 
+        published: args.published, 
+        genres: args.genres, 
+        author: author._id
+      })
+      await book.save()
+
       return book
     },
     editAuthor: (root, args) => {
